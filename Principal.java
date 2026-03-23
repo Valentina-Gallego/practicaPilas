9628500490cc726b2679d3dac36e939fb72fbff3
Valentina-Gallego-Ramos <144750192+Valentina-Gallego-Ramos@users.noreply.github.com>
2026-03-23 16:40:43 -0500

    ..

--- Principal.java
+++ Principal.java
@@ -8,32 +8,31 @@ public class Principal {
         Stack<objPaginaWeb> p = new Stack<>();
         boolean seguir = true;
         while (seguir) {
-    int opt = m.menu(sc);
+            int opt = m.menu(sc);
 
-    switch (opt) {
-        case 1:
-            m.LLenarPila(p, sc);
-            break;
+            switch (opt) {
+                case 1:
+                    m.LLenarPila(p, sc);
+                    break;
 
-        case 2:
-            m.MostrarPila(p);
-            break;
+                case 2:
+                    m.MostrarPila(p);
+                    break;
 
-        case 3:
-            m.Retroceder(p);
-            break;
+                case 3:
+                    m.Retroceder(p);
+                    break;
 
-        case 4:
-            System.out.println("SALIENDO.....");
-            seguir = false; // 👈 IMPORTANTE
-            break;
+                case 4:
+                    System.out.println("SALIENDO.....");
+                    seguir = false;
+                    break;
 
-        default:
-            System.out.println("INGRESE UNA OPCION DEL MENU");
-            break;
+                default:
+                    System.out.println("INGRESE UNA OPCION DEL MENU");
+                    break;
+            }
+        }
+
     }
 }
-
-        }
-    }
-
